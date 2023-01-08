fc50cf13502c7622f9cf98951ad1dc036b6a7d4d
Sandesh.Upadhyaya <Sandesh.Upadhyaya@cedargate.com>
2023-01-08 13:44:27 +0545

    checking javascript

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const test = new Function('var x = 10;\
-var y=20;\
-return \'there\';')
-console.log(test());
-
-// const str = "sdljl3\n2j4l23";
-// const replaced = str.replace(/([\r\n])/g, '\\$1');
-// const result = (new Function('return (replaced);'))();
-// console.log(result);
-
+function disable(){
+    var lets = document.getElementById("lets");
+    lets.style.display = 'none';
+}
