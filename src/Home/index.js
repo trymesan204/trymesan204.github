1a52cbfeef390ae6bb2560f0a858d3094daba564
Sandesh.Upadhyaya <Sandesh.Upadhyaya@cedargate.com>
2023-01-19 19:20:29 +0545

    introduction added

--- src/Home/index.js
+++ src/Home/index.js
@@ -6,7 +6,24 @@ const Home = () => {
     return (
         <>
             <div className='photo-background'>
-                <img src={pp} className='photo' alt="I, me and myself"/>
+                <div className='main-content'>
+                    <img src={pp} className='photo' alt='I, me and myself'/>
+                </div>
+            </div>
+            <div className='whole-content'>
+                <div className='main-content'>
+                    <div className='introduction'>
+                        <h1>Yo 🤪, I am Sandesh Prasad Upadhyaya </h1><br/>
+                        <p>
+                            I am a Computer Engineer graduated from Pulchowk Campus. I have 3+ years of experience
+                            in Software Development. I have worked on techstacks such as HTML/CSS/Javascript,
+                            ReactJs, NodeJs, .net, Groovy and Grails and Android Programming with Java. I have practice
+                            mainly of SQL databases like MySQL and SQLite.<br/><br/>
+                            Other than professional career, I like reading books. Once in a fortnite, I try to write
+                            but fail successfully. Lol. I also have a hobby of reading old Nepali literatures.
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -4,20 +4,40 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './Navigation.css';
 
-const Navigation = () => {
-  return (
-    <Navbar className="navbar" sticky="top" bg="white">
-      <Container className="container">
-        <Navbar.Brand href="/"><b>Sandesh Upadhyaya</b></Navbar.Brand>
-        <Nav className="space">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/blogs">Blog</Nav.Link>
-          <Nav.Link href="/projects">Projects</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
-}
+export default class Navigation extends React.Component {
 
-export default Navigation;
+  componentDidMount(){
+    window.addEventListener("scroll", this.resizeHeaderOnScroll);
+  }
+
+  resizeHeaderOnScroll(){
+    const distanceY = document.body.scrollTop || document.documentElement.scrollTop;
+    const shrinkOn = 100;
+    const navBar = document.getElementById("navbar");
+    console.log(distanceY);
+
+    if (distanceY > shrinkOn) {
+      navBar.style.height = "60px";
+      navBar.style.transition = "0.5s";
+    }else{
+      navBar.style.height = "150px";
+      navBar.style.transition = "0.5s";
+    }
+  }
+
+  render(){
+    return (
+      <Navbar id="navbar" className="navbar" sticky="top" bg="white">
+        <Container className="container">
+          <Navbar.Brand href="/"><b>Sandesh Upadhyaya</b></Navbar.Brand>
+          <Nav className="space">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/blogs">Blog</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+  }
+}
